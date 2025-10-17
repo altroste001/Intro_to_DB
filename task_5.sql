9348326a0eea5d17c4f726077915de4f7cdc5ff3
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-10-17 15:42:24 +0100

    Fix task 5: Correct address format

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,2 @@
-INSERT INTO customer (customer_id , customer_name ,email ,address )
-VALUES
-(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happines Ave.");
-
-SELECT * FROM customers;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
