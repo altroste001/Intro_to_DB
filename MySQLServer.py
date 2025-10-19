ce190c2a3a4b4bb63a9299a3de35012693e46c3a
Ayoub Bouabana <ayoubbouabana@gmail.com>
2025-10-19 18:58:07 +0100

    Add task 1: Create database with Python script

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            password="AYOUB2004"
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    create_database()
